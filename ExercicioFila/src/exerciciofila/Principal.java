379907235c9315e119aef112a8f72e8998e8a443
GiovanaMarsigli <marsigligiovana@gmail.com>
2025-03-19 21:35:24 -0300

    Update

---
+++ ExercicioFila/src/exerciciofila/Principal.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exerciciofila;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        Fila fila = new Fila();
+        fila.enfileira("A");
+        fila.enfileira("B");
+        fila.enfileira("C");
+
+        System.out.println("Mostrar fila:");
+        fila.imprime();
+
+        fila.desenfileira();
+        
+        System.out.println("\nFila após remoção:");
+        fila.imprime();
+    }
+}
